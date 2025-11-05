31967d186d2aa5e32c4976a6ea53791063e0b544
mapp-john <47927776+mapp-john@users.noreply.github.com>
2025-11-05 13:24:36 -0700

    Update pxdirect_pusher_by_id_update.py

--- pxdirect_pusher_by_id_update.py
+++ pxdirect_pusher_by_id_update.py
@@ -3,18 +3,19 @@ import json
 
 ise_pan = "1.1.1.1"
 connector_name = "Test-Pusher"
-unique_id = "55:55:55:55:55:55"
+unique_id = "00001"
 pxgrid_direct_user = "pxgrid_user"
 pxgrid_direct_password = "pxgrid_password"
 
 pxgrid_direct_url = f"https://{ise_pan}/api/v1/pxgrid-direct/push/{connector_name}/{unique_id}"
 
 pxgrid_direct_data = {
+    "id": "00001",
     "mac": "55:55:55:55:55:55",
     "hostname": "sjc-hv-01",
     "type": "iot",
     "role": "hvac",
-    "location": "New York",
+    "location": "San Diego",
 }
 
 r = requests.put(
