9b7810fcf1d8557ae497d5b60922de0241de7deb
mapp-john <47927776+mapp-john@users.noreply.github.com>
2025-11-05 09:15:24 -0700

    Add files via upload

---
+++ pxdirect_pusher_by_id_get.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+ise_pan = "1.1.1.1"
+connector_name = "Test-Pusher"
+unique_id = "66:66:66:66:66:66"
+pxgrid_direct_user = "pxgrid_user"
+pxgrid_direct_password = "pxgrid_password"
+
+pxgrid_direct_url = f"https://{ise_pan}/api/v1/pxgrid-direct/push/{connector_name}/{unique_id}"
+
+r = requests.get(
+    pxgrid_direct_url,
+    auth=(pxgrid_direct_user, pxgrid_direct_password),
+    verify=False,
+)
+
+# Raise an exception for HTTP errors
+r.raise_for_status()
+# Print response
+print(json.dumps(r.json(), indent=4))
